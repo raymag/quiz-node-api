33a6224d24b737e14d2282765a920fd53e1130a4
raymag <prayk11@outlook.com>
2020-02-10 11:00:58 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const routes = require('./routes') // includes the routes.js file
+const cors = require('cors') // includes cors module
 
 require('dotenv').config()
 
+app.use(cors()) // We're telling express to use CORS
 app.use(express.json()) // we need to tell server to use json as well
 app.use(routes) // tells the server to use the routes in routes.js
 
